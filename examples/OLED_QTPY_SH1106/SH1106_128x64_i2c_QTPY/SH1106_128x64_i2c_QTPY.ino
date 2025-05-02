8f0a9672477299ed4a92867d3a76f773f973be7f
Jonas Bechtel <bechtel@localhost.localdomain>
2025-05-02 07:14:29 +0200

    Device type of LCD module as comment for search engines

--- examples/OLED_QTPY_SH1106/SH1106_128x64_i2c_QTPY/SH1106_128x64_i2c_QTPY.ino
+++ examples/OLED_QTPY_SH1106/SH1106_128x64_i2c_QTPY/SH1106_128x64_i2c_QTPY.ino
@@ -24,6 +24,7 @@
 
 /* Uncomment the initialize the I2C address , uncomment only one, If you get a totally blank screen try the other*/
 #define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
+                         // e.g. the one with GM12864-77 written on it
 //#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
